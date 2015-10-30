5988a9d0ac88be366028183e214412ac7d2112dc
felixyiphone <yinxianjin66@163.com>
2015-10-30 16:37:48 +0800

    百度推送测试版提交

---
+++ 开发版Demo/BPushDevDemo/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  BPushDemo
+//
+
+// UDID: ba46b8edda9d6d52eba2bb7359440915db2a78ae
+
+// UDID: ba46b8edda9d6d52eba2bb7359440915db2a78ae
+
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
